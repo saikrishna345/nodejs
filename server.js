c7e2d1cfd18b09408e1f830bc0924413a033fd35
saikrishna345 <saikrishna345>
2019-05-13 09:39:33 +0530

    added node js files

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//  var cors = require('cors');
+//  var corsOptions = {
+//      origin: "http://localhost:4200",
+//      optionsSuccessStatus: 200
+//  }
+//  app.use(cors(corsOptions));
+
+var mysqlconnection = mysql.createConnection({
+host: 'localhost',
+user:'root',
+port: 3306,
+password: 'krish',
+database: 'mydatabase'
+})
+
+mysqlconnection.connect((err) =>{
+    if(err){
+        console.log('Connection unavailable');
+    } else{
+        console.log('Db connection succeeded');
+    }
+})
+
+
+app.listen(3000,()=> console.log('server listening to port number 3000'));
+
+app.get('/employees',function(req,res){
+ mysqlconnection.query('SELECT * FROM employee',(err,rows,fields)=>{
+if(!err){
+    console.log(rows);
+}
+else{
+    console.log(err);
+}
+ })
+
+})
